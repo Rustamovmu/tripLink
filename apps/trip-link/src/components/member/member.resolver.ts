2686ca283a7a043f3412af15c43a9481c076c718
M.U <rustamovmuhammadumar2004@gmail.com>
2026-09-13 20:58:05 +0900

    feat: add member authentication check

--- apps/trip-link/src/components/member/member.resolver.ts
+++ apps/trip-link/src/components/member/member.resolver.ts
@@ -1,6 +1,9 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { AuthPayload } from '../../libs/dto/member/member';
+import { AuthMember } from '../auth/decorators/auth-member.decorator';
+import { AuthGuard } from '../auth/guards/auth.guard';
 import { MemberService } from './member.service';
 
 @Resolver()
@@ -15,5 +18,11 @@ export class MemberResolver {
 	@Mutation(() => AuthPayload)
 	public login(@Args('input') input: LoginInput): Promise<AuthPayload> {
 		return this.memberService.login(input);
+	}
+
+	@UseGuards(AuthGuard)
+	@Query(() => String)
+	public checkAuth(@AuthMember('memberNick') memberNick: string): string {
+		return `Hi ${memberNick}`;
 	}
 }
